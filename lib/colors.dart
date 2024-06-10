b3056d2b482c9daec9f85e5894991ca740c251fb
Lars Dieper <ldieper1@gmail.com>
2024-06-10 13:31:58 +0200

    added firebase hosting capabilities

---
+++ lib/colors.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+// Spielfeld
+const tomato = Color.fromARGB(255, 255, 99, 71);
+const tomato2 = Color.fromARGB(255, 223, 106, 85);
+const springgreen = Color.fromARGB(255, 0, 255, 127);
+const springgreen2 = Color.fromARGB(255, 32, 223, 127);
+const skyblue = Color.fromARGB(255, 135, 206, 235);
+const skyblue2 = Color.fromARGB(255, 133, 186, 208);
+const backgroundblue = Color.fromARGB(240, 169, 216, 255);
+const fieldbackground = Color.fromARGB(50, 128, 128, 128);
+
+// Baumphase
+const cornsilk = Color.fromARGB(255, 255, 248, 220);
+const rebeccapurple = Color.fromARGB(255, 102, 51, 153);
+const royalblue = Color.fromARGB(255, 65, 105, 225);
+const yellowgreen = Color.fromARGB(255, 154, 205, 50);
+const hotpink = Color.fromARGB(255, 255, 105, 180);
+
+// Gruppenphase Übersicht
+const steelblue = Color.fromARGB(255, 70, 130, 180);
+const grouppurple = Color.fromARGB(240, 180, 70, 130);
+const cupred = Color.fromARGB(255, 213, 35, 70);
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pongstrong/colors.dart';
 
 Future<void> main() async {
   // initialize Firebase
@@ -195,25 +196,3 @@ void addTestTeam() {
   //  'member2': 'Rudi',
   //});
 }
-
-// Spielfeld
-const tomato = Color.fromARGB(255, 255, 99, 71);
-const tomato2 = Color.fromARGB(255, 223, 106, 85);
-const springgreen = Color.fromARGB(255, 0, 255, 127);
-const springgreen2 = Color.fromARGB(255, 32, 223, 127);
-const skyblue = Color.fromARGB(255, 135, 206, 235);
-const skyblue2 = Color.fromARGB(255, 133, 186, 208);
-const backgroundblue = Color.fromARGB(240, 169, 216, 255);
-const fieldbackground = Color.fromARGB(50, 128, 128, 128);
-
-// Baumphase
-const cornsilk = Color.fromARGB(255, 255, 248, 220);
-const rebeccapurple = Color.fromARGB(255, 102, 51, 153);
-const royalblue = Color.fromARGB(255, 65, 105, 225);
-const yellowgreen = Color.fromARGB(255, 154, 205, 50);
-const hotpink = Color.fromARGB(255, 255, 105, 180);
-
-// Gruppenphase Übersicht und Login
-const steelblue = Color.fromARGB(255, 70, 130, 180);
-const grouppurple = Color.fromARGB(240, 180, 70, 130);
-const cupred = Color.fromARGB(255, 213, 35, 70);
